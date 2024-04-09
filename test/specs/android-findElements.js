dad4addfd066de98af394b39b86722b2fc4f2f94
Ani <temp-anirudho.sengupta@cafu.com>
2024-04-09 11:53:24 +0530

    second commit

--- test/specs/android-findElements.js
+++ test/specs/android-findElements.js
@@ -1,5 +1,7 @@
+const { click } = require("appium-uiautomator2-driver/build/lib/commands/element");
+
 describe('Android Element Tests',()=>{
-it('find element by Accessibiloity id',async()=>{
+it('find element by Accessibility id',async()=>{
         //Accessibility ID
 
        const appOption  = await $('~App');
@@ -8,6 +10,20 @@ it('find element by Accessibiloity id',async()=>{
        const actionBar = await $('~Action Bar');
 
        await expect(actionBar).toExist();
-})
+});
+
+it('find element by Class Name',async()=>{
+        const className = await $('android.widget.TextView');
+
+        console.log(await className.getText());
+
+        await expect(className).toHaveText("API Demos")
+
+});
+
+it('find element by xpath',async()=>{
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click();
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click();
+});
 
 });
