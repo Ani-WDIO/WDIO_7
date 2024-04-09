957d9395b6fb1d57661e14c20df9e6829459c769
Ani <temp-anirudho.sengupta@cafu.com>
2024-04-09 17:58:02 +0530

    Alerts and dialog Box

--- test/specs/android-native.js
+++ test/specs/android-native.js
@@ -7,4 +7,38 @@ describe('',()=>{
 
           await expect($('//*[@text="App/Alert Dialogs"]')).toExist();
     });
+
+    it('Accepting Dialog Boxes',async()=>{
+
+        await driver.startActivity("io.appium.android.apis","io.appium.android.apis.app.AlertDialogSamples");
+          
+        await $('//*[resource-id="io.appium.android.apis:id/two_buttons"]').click();
+
+        await driver.acceptAlert();
+
+        await expect($('//*[resource-id="android:id/alertTitle"]')).not.toExist();
+    });
+
+    it('Dismissing Dialog Boxes',async()=>{
+
+        await driver.startActivity("io.appium.android.apis","io.appium.android.apis.app.AlertDialogSamples");
+          
+        await $('//*[resource-id="io.appium.android.apis:id/two_buttons"]').click();
+
+        await driver.dismissAlert();
+
+        await expect($('//*[resource-id="android:id/alertTitle"]')).not.toExist();
+    });
+
+    it.only('Get text from Dialog Boxes',async()=>{
+
+        await driver.startActivity("io.appium.android.apis","io.appium.android.apis.app.AlertDialogSamples");
+          
+        await $('//*[resource-id="io.appium.android.apis:id/two_buttons"]').click();
+
+        console.log("Alert Text ",await driver.getAlertText());
+
+        await expect($('//*[resource-id="android:id/alertTitle"]')).not.toExist();
+    });
+
 })
